239c772c9eb7b98b6270a3bfc18d921486a15ae8
zeos888 <voevodin.alexey@gmail.com>
2018-03-06 01:58:53 +0200

    enhanced & corrected

--- current.js
+++ current.js
@@ -18,19 +18,13 @@ ws.on('open', function open() {
     ws.send(JSON.stringify(ticker));
 });
 
-ws.on('close', function close() {
-    //console.log('disconnected');
-});
-
 ws.on('message', function incoming(data) {
     let j = JSON.parse(data);
 
-    if (j.data && j.data["symbol1"] == 'BTC' && j.data["symbol2"] == 'USD'){
+    if (j.data && j.data["symbol1"] == 'BTC' && j.data["symbol2"] == 'USD') {
         if (j.data['price'] != current_price) {
             current_price = j.data['price'];
             eventEmitter.emit("priceChange", current_price);
-            //console.log(current_price);
         }
     }
-    //console.log(j.data);
 });
--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-const request = require('request');
 const moment = require('moment');
 
 const latest = require('./latest');
 const current = require('./current');
 
+const config = require('./config.json');
+
 let current_price;
 let latest_price;
 let percent;
 
-// latest('https://api.coinmarketcap.com/v1/ticker/bitcoin/', (resp) => {
-//     current_price = resp;
-//     console.log(current_price);
-// });
-
-(function() {
-    var c = 0;
-    var timeout = setInterval(function() {
+(function () {
+    setInterval(function () {
         latest((resp) => {
             latest_price = resp;
-            console.log("latest = " + latest_price);
         });
-    }, 10000);
+    }, 1000 * config.timeout);
 })();
 
 current.on("priceChange", (data) => {
     current_price = data;
-    console.log("current = " + current_price);
-    if (latest_price && percent != (current_price / latest_price)/100){
-        percent = (current_price / latest_price)/100;
-        let now = moment.utc().format("mm:ss");
-        console.log("now, on " + now + " percent = " + percent);
+    if (latest_price){
+        let new_percent = (((current_price - latest_price) / latest_price) / 100).toFixed(config.decimalPoints);
+        if (percent != new_percent) {
+            percent = new_percent;
+            let now = moment.utc().format(config.dateFormat);
+            console.log(now + " " + percent + "%");
+        }
     }
 });
